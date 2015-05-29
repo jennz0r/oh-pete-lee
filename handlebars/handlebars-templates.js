57c3732ffb16bfe368a76dbc331d68aaf7eccc46
jennz0r <jenn.ew@gmail.com>
2015-05-28 20:56:05 -0700

    added video modal

--- handlebars/handlebars-templates.js
+++ handlebars/handlebars-templates.js
@@ -33,20 +33,24 @@ this["JST"]["handlebars/photography.hbs"] = Handlebars.template({"compiler":[6,"
 this["JST"]["handlebars/videography.hbs"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<!--\n     --><li>\n    		<div class=\"video__outer\">\n    			<!-- <iframe src=\"https://player.vimeo.com/video/"
-    + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
-    + "?badge='0'&byline='0'&color='f2002b'&portrait='0'&title='0'\" frameborder=\"0\" width=\"500\" height=\"281\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->\n          <img src=\""
+  return "<!--\n   --><li>\n        <div class=\"video__outer\">\n          <img src=\""
     + alias3(((helper = (helper = helpers.thumbnailUrl || (depth0 != null ? depth0.thumbnailUrl : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"thumbnailUrl","hash":{},"data":data}) : helper)))
-    + "\">\n    			<div class=\"video__overlay\">\n    				<p><a href=\"https://vimeo.com/"
+    + "\">\n          <div class=\"video__overlay\" data-reveal-id=\"videoModal"
+    + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\">\n            <p><a href=\"https://vimeo.com/"
     + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
     + "\" target=\"_blank\">"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</a> from <a href=\"https://vimeo.com/petelee\" target=\"_blank\">Peter Lee</a></p>\n    			</div>\n    		</div>\n    	</li><!--\n    -->";
+    + "</a></p>\n          </div>\n        </div>\n      </li>\n      <div id=\"videoModal"
+    + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\"reveal-modal large\" data-reveal aria-labelledby=\"videoModalTitle\" aria-hidden=\"true\" role=\"dialog\">\n        <h2 id=\"videoModalTitle\">This modal has video</h2>\n        <div class=\"flex-video widescreen vimeo\">\n          <iframe src=\"https://player.vimeo.com/video/"
+    + alias3(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"id","hash":{},"data":data}) : helper)))
+    + "?badge='0'&byline='0'&color='f2002b'&portrait='0'&title='0'\" frameborder=\"0\" width=\"500\" height=\"281\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>\n        </div>\n        <a class=\"close-reveal-modal\" aria-label=\"Close\">&#215;</a>\n      </div><!--\n    -->";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
   return ((stack1 = this.invokePartial(partials.nav,depth0,{"name":"nav","data":data,"helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "\n<div class=\"row\">\n  <div class=\"columns\">\n    <ul class=\"small-block-grid-1  medium-block-grid-2  large-block-grid-3  mv4x\">\n    	"
+    + "\n<div class=\"row\">\n  <div class=\"columns\">\n    <ul class=\"small-block-grid-1  medium-block-grid-2  large-block-grid-3  mv4x\">\n      "
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.videos : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "\n    </ul>\n  </div>\n</div>";
 },"usePartial":true,"useData":true});
