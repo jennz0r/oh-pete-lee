e8807763f48af7bde48042559b4a8211576bc3b1
jennz0r <jenn.ew@gmail.com>
2015-08-13 20:44:20 -0700

    tiny change to background size

--- handlebars/handlebars-templates.js
+++ handlebars/handlebars-templates.js
@@ -24,7 +24,7 @@ this["JST"]["handlebars/contact.hbs"] = Handlebars.template({"compiler":[6,">= 2
     var stack1, helper;
 
   return ((stack1 = this.invokePartial(partials.nav,depth0,{"name":"nav","data":data,"helpers":helpers,"partials":partials})) != null ? stack1 : "")
-    + "\n<div class=\"row  mv  ph\">\n  <div class=\"small-12  medium-6  large-5  small-centered  medium-uncentered  columns\">\n    <h1 class=\"title\">"
+    + "\n<div class=\"row  mv  ph\">\n  <div class=\"small-12  medium-5  small-centered  medium-uncentered  columns\">\n    <h1 class=\"title\">"
     + this.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h1>\n    <p class=\"bio  mv\">\n      <span><a href=\"http://enterthehero.com/\">Enter the Hero</a></span><br>\n      <span><a href=\"mailto:pete@ohpetelee.com\">pete@ohpetelee.com</a></span><br>\n      <span>617.784.1886</span>\n    </span><br>\n  </div>\n</div>";
 },"usePartial":true,"useData":true});
